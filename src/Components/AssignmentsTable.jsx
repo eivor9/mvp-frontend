8db05610789bd68f53ea88e69da33c51e0834c4f
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-09-27 16:02:53 -0400

    File Structure Setup with File

---
+++ src/Components/AssignmentsTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AssignmentsTable = () => {
+  return (
+    <div>AssignmentsTable</div>
+  )
+}
---
+++ src/Components/ProgressTrackingMetrics.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProgressTrackingMetrics = () => {
+  return (
+    <div>ProgressTrackingMetrics</div>
+  )
+}
