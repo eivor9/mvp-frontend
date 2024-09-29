a9c7785fb58178d47f15837739591d130554c31b
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-09-29 00:58:55 -0400

    Implement Footer component with styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,21 @@
 // Components/Footer.jsx
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className="footer">
+      <nav className="footer-nav">
+        <Link to="/" className="footer-link"> MVP </Link> |
+        <Link to="/about" className="footer-link"> About </Link> |
+        <Link to="/link1" className="footer-link"> Link 1 </Link> |
+        <Link to="/link2" className="footer-link"> Link 2 </Link> |
+        <Link to="/link3" className="footer-link"> Link 3 </Link>
+      </nav>
+      <p className="footer-copyright"> &copy; 2024 Mentor Volunteer Platform. All rights reserved. </p>
+    </footer>
   );
 };
 
