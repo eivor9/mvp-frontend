f6b8b31a0c7cd7663ea4ac4302db5345f6177f2c
JayZ44 <jazonyounge@pursuit.org>
2024-09-29 15:20:58 -0400

    added .env to .gitignore, added about page to the list of routes in app.jsx, added about page to pages folder.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 // DEPENDENCIES
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 
 // PAGES
 import Home from "./Pages/Home";
@@ -11,6 +11,7 @@ import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
 import Category from "./Pages/Category";
 import FourOFour from "./Pages/FourOFour";
+import About from "./Pages/About";
 
 // COMPONENTS
 import NavBar from "./Components/NavBar";
@@ -18,23 +19,24 @@ import Footer from "./Components/Footer";
 
 function App() {
   return (
-   <div className="App">
-    <Router>
-      <NavBar/>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          {/* Categories Routes */}
-          <Route path="*" element={<FourOFour />} />
-        </Routes>
-      </main> 
-      <Footer/>
-    </Router>
-   </div>
-  )
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/about" element={<About />} />
+            {/* Categories Routes */}
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h3>Our Mission</h3>
+      <strong>
+        {" "}
+        <p>
+          To eliminate financial and organizational obstacles, enabling mentors
+          and mentees to connect, collaborate, and grow together.
+        </p>{" "}
+      </strong>
+    </div>
+  );
+};
+
+export default About;
