42a8553763d2f87f2e6bbe3e6bbbd43da533077f
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-09-30 13:06:29 -0400

    Add 404 page with footer styling

--- src/Pages/FourOFour.jsx
+++ src/Pages/FourOFour.jsx
@@ -1,11 +1,13 @@
 // Pages/FourOFour.jsx
 
 import React from 'react';
+import '../Styles/Footer.css';
 
 const FourOFour = () => {
   return (
-    <div>
-      
+    <div className='footer'>
+      <h1> 404 - Page Not Found </h1>
+      <p> Sorry, the page you are looking for does not exist. </p>
     </div>
   );
 };
