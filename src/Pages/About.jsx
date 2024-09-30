a74907206b8f75214237d708e9145f5917bd7f8e
JayZ44 <jazonyounge@pursuit.org>
2024-09-30 12:30:28 -0400

    added content to about component and it's css file

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,40 +1,42 @@
 import React from "react";
-import "./About.css";
+import "../Styles/About.css";
 const About = () => {
   return (
     <div>
       <h1>About</h1>
-      <h3>Our Mission</h3>
-      <strong>
-        {" "}
-        <p>
-          To eliminate financial and organizational obstacles, enabling mentors
-          and mentees to connect, collaborate, and grow together.
-        </p>{" "}
-      </strong>
-      <h5>
-        MVP is a dynamic and groundbreaking platform that enables mentors and
-        mentees to foster each other's grouth through personal development
-      </h5>
-      <h3>For Mentees</h3>
-      <strong>
-        {" "}
-        <p>
+      <div className="text-box">
+        <h3>Our Mission</h3>
+        <strong>
           {" "}
-          To eliminate financial and organizational obstacles, enabling mentors
-          and mentees to connect, collaborate, and grow together.
-        </p>{" "}
-      </strong>
-      <h3>For Mentors</h3>
-      <strong>
-        {" "}
-        <p>
+          <p>
+            To eliminate financial and organizational obstacles, enabling
+            mentors and mentees to connect, collaborate, and grow together.
+          </p>{" "}
+        </strong>
+        <h5>
+          MVP is a dynamic and groundbreaking platform that enables mentors and
+          mentees to foster each other's grouth through personal development
+        </h5>
+        <h3>For Mentees</h3>
+        <strong>
           {" "}
-          To eliminate financial and organizational obstacles, enabling mentors
-          and mentees to connect, collaborate, and grow together.
-        </p>{" "}
-      </strong>
-      <h3>Contributors</h3>
+          <p>
+            {" "}
+            To eliminate financial and organizational obstacles, enabling
+            mentors and mentees to connect, collaborate, and grow together.
+          </p>{" "}
+        </strong>
+        <h3>For Mentors</h3>
+        <strong>
+          {" "}
+          <p>
+            {" "}
+            To eliminate financial and organizational obstacles, enabling
+            mentors and mentees to connect, collaborate, and grow together.
+          </p>{" "}
+        </strong>
+        <h3>Contributors</h3>
+      </div>
       <div className="pic-container">
         <img src="" alt="" className="dev-pic" />
         <img src="" alt="" className="dev-pic" />
