bbd2a993237bf6d17951a9a58740cd2051a0f49d
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-10-03 16:16:25 -0400

    feat: Update Testimonials component to fetch data from backend
    - Implemented fetching of testimonials from the backend API.
    - Displayed category names, mentor IDs, mentee IDs, and review titles in the Testimonials component.
    - Added error handling for fetch failures.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,12 @@
 
 import React from "react";
 import CategoriesCarousel from "../Components/CategoriesCarousel";
-
+import Testimonials from "../Components/Testimonials";
 const Home = () => {
   return (
     <div>
       <CategoriesCarousel />
+      <Testimonials />
     </div>
   );
 };
