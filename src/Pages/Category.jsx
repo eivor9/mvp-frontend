82be51a06a755d9a622deda2dcca4d3fc2dbee1c
AAkinkunmi <aakinkunmi@pursuit.org>
2024-10-09 01:07:52 -0400

    build category page and connect to backend

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -5,6 +5,7 @@ import "../Styles/Category.css";
 
 //components 
 import UserCard from '../Components/UserCard';
+import Hero from '../Components/Hero';
 
 const Category = () => {
 
@@ -34,42 +35,72 @@ const Category = () => {
     .catch(err => {
       console.error(err);
     })
-  }, [category_id, API])
+  }, [category_id, API]);
 
   const toggleShowMentors = () => {
-    setShowMentors(!showMentors);
-  }
+    setShowMentors(true);
+  };
 
-  console.log({mentors, mentees})
+  const toggleShowMentees = () => {
+    setShowMentors(false);
+  };
+
+  const toggleHeaderColor = () => {
+    return {backgroundColor: '#222850', color: "white"};
+}
+
+  console.log({mentors, mentees});
 
   return (
-    <div>
-      <div className="header">
-        <span onClick={toggleShowMentors}>Mentors</span><span onClick={toggleShowMentors}>Mentees</span>
+    <div className='category-container'>
+      <Hero />
+      <div className="toggle-header">
+        <h3 id='mentor-header' style={showMentors ? toggleHeaderColor() : null} onClick={toggleShowMentors}>Mentors</h3>
+        <h3 id='mentee-header' style={!showMentors ? toggleHeaderColor() : null} onClick={toggleShowMentees}>Mentees</h3>
       </div>
 
-      <div className="category-user-list">
-      {showMentors ? (mentors.map(mentor => {
-        return  <Link key={mentor.id}>
-                  <UserCard
-                    name={`${mentor.first_name} ${mentor.last_name}`}
-                    job_title={`${mentor.job_title}`}
-                    categoryName={categoryName}
-                    className="category-card"
-                  />
-                </Link>        
-      })) : (mentees.map(mentee => {
-        return  <Link key={mentee.id}>
-                  <UserCard
-                    name={`${mentee.first_name} ${mentee.last_name}`}
-                    job_title={`${mentee.job_title}`}
-                    categoryName={categoryName}
-                    className="category-card"
-                  />
-                </Link>
-        
-      }))}
-      </div>
+      {showMentors && (
+        <div className="category-user-list">
+          {mentors.length > 0 ?
+              (mentors.map(mentor => {
+                  return  <Link key={mentor.id}>
+                            <UserCard
+                              name={`${mentor.first_name} ${mentor.last_name}`}
+                              job_title={`${mentor.job_title}`}
+                              categoryName={categoryName}
+                              className="category-card"
+                            />
+                          </Link>        
+                })) : (
+                <p>No mentors available in this category</p>
+              )
+          }
+        </div>
+      )}
+      
+       {!showMentors && (
+        <div className="category-user-list">
+          {mentees.length > 0 ? 
+                (mentees.map(mentee => {
+                  return  <Link key={mentee.id}>
+                            <UserCard
+                              name={`${mentee.first_name} ${mentee.last_name}`}
+                              job_title={`${mentee.job_title}`}
+                              categoryName={categoryName}
+                              className="category-card"
+                            />
+                          </Link>
+                  
+                })) : (
+                <p>No mentees in this category</p>
+                )
+          }
+        </div>
+       )
+      
+       
+      }
+      
     </div>
   );
 };
