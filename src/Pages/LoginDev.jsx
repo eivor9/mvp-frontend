3cce518fcd0a056e16debff460adfdd6e855bb8e
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-10-18 15:46:17 -0400

    Fix sign-up connection issues causing registration failures

--- src/Pages/LoginDev.jsx
+++ src/Pages/LoginDev.jsx
@@ -1,7 +1,6 @@
-// Pages/Login.jsx
+// Pages/LoginDev.jsx
 
 import React, { useState } from 'react';
-import { Box, Button, TextField, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import "../Styles/Login.css";
